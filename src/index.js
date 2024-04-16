5acfa55591d860052f24f87cae9dfbbb7ca90dcc
tiffanyandjia <104477148+tiffanyandjia@users.noreply.github.com>
2024-04-16 17:35:11 -0400

    add some function on the index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Footer />
+    </div>
+  );
 }
 
+function Header() {
+  //const style = {color: "red", fontSize:"48px", textTransform:"uppercase"};
+  const style = {};
+  return <h1>Fast Ract Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleDateString()}We're currently open</footer>;
+}
 //react v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
